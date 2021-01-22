95a2443e1ec7de8690c414b63a38465f3a4cbf71
msd <msd>
2021-01-23 02:27:41 +0800

    dabao

--- webpack.config.pro.js
+++ webpack.config.pro.js
@@ -8,7 +8,7 @@ const path = require("path");
 const proConfig = {
   mode: "production",
   output: {
-    path: path.resolve(__dirname, "./dist"),
+    path: path.resolve(__dirname, "./docs"),
     filename: "[name].js",
     // publicPath: "http://... ...",
   },
