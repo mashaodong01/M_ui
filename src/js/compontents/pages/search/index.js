182f0804b90bdc5f1a7b5fb77b14996c3086a50d
mashaodong15777 <mashaodong15777@ipalfish.com>
2021-09-21 15:23:25 +0800

    feat: 鼠标预测技术

--- src/js/compontents/pages/search/index.js
+++ src/js/compontents/pages/search/index.js
@@ -1,6 +1,11 @@
 import Search from "./search.js";
 import _style from "./style.js";
 import _template from "./template.js";
+function click() {
+  import("./template.js").then(data => {
+    console.log(data.default);
+  })
+}
 
 const template = document.createElement("template");
 template.innerHTML = `
